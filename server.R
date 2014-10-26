9cb6d40cfb523a2cee258ad33a8a150bec02aef7
hmbiyangandu <hmbiyangandu@yahoo.fr>
2014-10-26 14:37:08 +0100

    Updates

--- server.R
+++ server.R
@@ -1,9 +1,42 @@
 library(shiny)
-shinyServer(function(input, output){
-  output$text1 <- renderText({
-    paste("You have selected", input$var)
-  })
-  output$text2 <- renderText({
-    paste("You have chosen a range that goes from", input$range[1], "to", input$range[2])
-  })
-})
+calculateArea <- function(s,n){
+  0.25*n*(s^2)/tan(pi/n)  
+}
+
+calculatePerimeter <- function(s,n){
+  n*s 
+}
+
+shinyServer(
+  function(input, output){
+    output$sUnit <- renderText({input$sUnit})
+    
+    polygonInput <- reactive({
+      switch(input$nside,
+          "3"="Triangle",
+          "4"="Square",
+          "5"="Pentagon",
+          "6"="Hexagon",
+          "7"="Heptagon",
+          "8"="Octogone",
+          "9"="Nanogon",
+          "10"="Decagon"
+      )
+    })
+    
+    area <- reactive({calculateArea(input$side, as.numeric(input$nside))})
+    
+    perimeter <-reactive({calculatePerimeter(input$side, as.numeric(input$nside))})
+
+    output$text <- renderText({
+      paste("A(n)", polygonInput(), "with a side of", input$side, input$sUnit, ", has :"," ")}
+    )
+    output$areaText <- renderText({
+      paste("Area = ",round(area(),2)," ",input$sUnit,"^2",sep="")
+    })
+    output$perimeterText <- renderText({
+      paste("Perimeter = ",perimeter(), input$sUnit)
+    })
+        
+  }
+)
--- ui.R
+++ ui.R
@@ -1,20 +1,74 @@
 library(shiny)
-shinyUI(fluidPage(
-  titlePanel("censusVis"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create demographic maps with information from the 2010 US Census."),
-      br(),
-      selectInput("var", label="Choose a variable to display",
-                  choices=c("Percent White", "Percent Black", "Percent Indian", "Percent Asian"),
-                  selected="Percent Black"
+shinyUI(
+  fluidPage(
+    #Title of App
+    titlePanel("Polygon Area/Perimeter Calulator"),
+    
+    #Sidebar
+    sidebarLayout(
+      sidebarPanel(
+        helpText(em("Note: This application Calculate the Area and the Perimeter of regular Polygons, given the side lenght.",
+                    a(href="help.html", "More details here."))
+        ),
+        br(),
+        selectInput("nside",h5("Select a Polygon :"), 
+                    choices = c("Triangle"=3, 
+                    "Square"=4,
+                    "Pentagon"=5,
+                    "Hexagone"=6,
+                    "Heptagon"=7,
+                    "Octogon"=8,
+                    "Nonagon"=9,
+                    "Decagon"=10),3),
+        numericInput("side",h5("Enter side lenght :"), min=1, value=1),
+        br(),
+        
+        radioButtons("sUnit",h5("Unit"),
+                     choices=c("cm (centimeters)"="cm",
+                               "m (meters) "="m",
+                               "in (inches)"="in",
+                               "ft (feet)"="ft"),"m"),
+        submitButton(text="Calculate")
       ),
-      sliderInput("range",label="Range of interest :", min=0, max=100, value=c(0,100))
-    ),
-    mainPanel(
-      textOutput("text1"),
-      textOutput("text2")
-    )
-  )
-  
+      
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Results",
+                   hr(),
+                   p(textOutput("text"),
+                     br(),
+                     strong(
+                       tags$ul(
+                         tags$li(textOutput("areaText")),
+                         tags$li(textOutput("perimeterText"))
+                       )
+                     )
+                   )
+                   
+          ),
+          tabPanel("Documentation", 
+              p(
+                  br(tags$b("Polygon Calculator")),
+                  br("Use this calculator to calculate the Area and the Perimeter of commons regular",
+                     br(),
+                     " polygons (Tringle, Square, ..., Decagon)."
+                  ),                 
+                  br("To use the calculator :"),
+                  br(),                  
+                  tags$ol(
+                    tags$li("Select the polygon name,"), 
+                    tags$li("Enter the length of the side,"),
+                    tags$li("Choose the Units in which the area and the perimeter will be calculated",
+                            br(),
+                            "(Note: this doen't influence on the computation, it is just for convenience.)"
+                    ), 
+                    tags$li("Press the button ",span("Calculate", style="color:blue"), "to get the result.")
+                  ),
+                  br("The results of computation are displayed in", tags$b("Results panel.")),
+                  br("Note : By default when the App loads, the side is set to 1 and the polygon is Triangle.")
+              )
+        )
+      )
+    )  
+)
 ))
